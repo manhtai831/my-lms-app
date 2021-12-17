119218833daf93e4e8005fca402ed5cd70d7d2d7
Mạnh Tài <taidm>
2021-12-18 00:59:55 +0700

    commit - taidm

--- app/src/main/java/com/poly/lmsapp/commons/resource/StringResource.java
+++ app/src/main/java/com/poly/lmsapp/commons/resource/StringResource.java
@@ -4,6 +4,7 @@ public class StringResource {
     public static final String baseUrl = "https://lms-app-281.herokuapp.com/";//global
 //    public static final String baseUrl = "http://192.168.1.26:3002/";//home
 //    public static final String baseUrl = "http://192.168.1.39:3002/";//company
+
     public static  String token = "";
 
 }
